fc681cff41e837107167e482caad16b16416ab97
youngdeer <young_deer@sina.com>
2018-01-31 17:00:25 +0800

    hello deer

--- src/main/java/com/deer/jenkinsdeer/JenkinsDeerApplication.java
+++ src/main/java/com/deer/jenkinsdeer/JenkinsDeerApplication.java
@@ -2,11 +2,20 @@ package com.deer.jenkinsdeer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class JenkinsDeerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JenkinsDeerApplication.class, args);
+	}
+	
+	@RequestMapping("/hellodeer")
+	public String hellodeer(@RequestParam String name) {
+		return "hi deer,i am "+name;
 	}
 }
