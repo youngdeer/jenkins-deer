b698ce7b1094ca8c9e47c1d8af498f81f5d9dc69
youngdeer <young_deer@sina.com>
2018-01-31 16:11:02 +0800

    init

---
+++ src/main/java/com/deer/jenkinsdeer/JenkinsDeerApplication.java
@@ -0,0 +1,12 @@
+package com.deer.jenkinsdeer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsDeerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsDeerApplication.class, args);
+	}
+}
