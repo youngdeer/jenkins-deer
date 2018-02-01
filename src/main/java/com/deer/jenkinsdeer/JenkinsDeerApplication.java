ee863777e6ae480a7ffa432c6f5494e7bb9febad
youngdeer <young_deer@sina.com>
2018-02-01 09:23:30 +0800

    add annotation

--- src/main/java/com/deer/jenkinsdeer/JenkinsDeerApplication.java
+++ src/main/java/com/deer/jenkinsdeer/JenkinsDeerApplication.java
@@ -6,6 +6,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * 
+ * @author youngdeer
+ * springboot project run class
+ */
 @SpringBootApplication
 @RestController
 public class JenkinsDeerApplication {
