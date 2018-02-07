59355dfe46e91ad44d0f180e7e97226f6ae312e1
youngdeer <young_deer@sina.com>
2018-02-07 14:34:04 +0800

    change package to a deploy war

--- src/main/java/com/deer/jenkinsdeer/JenkinsDeerApplication.java
+++ src/main/java/com/deer/jenkinsdeer/JenkinsDeerApplication.java
@@ -4,6 +4,8 @@ import java.util.Map;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,7 +17,12 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @SpringBootApplication
 @RestController
-public class JenkinsDeerApplication {
+public class JenkinsDeerApplication extends SpringBootServletInitializer{
+	
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(JenkinsDeerApplication.class);
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(JenkinsDeerApplication.class, args);
